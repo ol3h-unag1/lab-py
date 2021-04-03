2d2ee4119ca73b49875e4936550c8dd254da0e63
MorningBlueOfficial <32937411+Cynacyn@users.noreply.github.com>
2021-04-03 13:35:49 +0300

    Plural and Fib iterators

--- DiveIntoPython.py
+++ DiveIntoPython.py
@@ -32,6 +32,10 @@ def to_bool( anything ):
 
 ###################################################
 ###################################################
+words = ('hex', 'play', 'beeacon',
+         'joy', 'holly', 'cloth', 'catch',
+         'teeth', 'cat', 'noise',
+         'employee', 'software')
 
 import re # !build_match_apply_function
 
@@ -44,22 +48,91 @@ def build_match_apply_function(pattern, search, replace):
 
    return (matches_rule, apply_rule)
 
-def rules(file_path):
-   with open(file_path, encoding = 'utf-8') as plur_pattern_file:
-      for line in plur_pattern_file:
-         pattern, search, replace = line.split(None,3)
-         yield build_match_apply_function(pattern, search, replace)
+#def rules(file_path):
+#   with open(file_path, encoding = 'utf-8') as plur_pattern_file:
+#      for line in plur_pattern_file:
+#         pattern, search, replace = line.split(None,3)
+#         yield build_match_apply_function(pattern, search, replace)
 
-import os # !plural
 
-plur_patterns_filename = 'pluralization_rules.txt'
+import os # !LazyRules
 
-def plural(noun, patterns_file_path = os.path.join( os.path.dirname(os.path.realpath(__file__)), plur_patterns_filename)):
-   for match_rule, apply_rule in rules(patterns_file_path):
+class LazyPlurRules:  
+
+   def __init__(self, rules_filename = 'pluralization_rules.txt'):
+      self.patten_file = open(os.path.join(os.path.dirname(os.path.realpath(__file__)), rules_filename), encoding='utf-8')
+      self.cache = []
+   
+   def __iter__(self):
+      self.cache_index = 0
+      return self
+
+   def __next__(self):
+      self.cache_index += 1
+      if len(self.cache) >= self.cache_index:
+         return self.cache[self.cache_index - 1]
+
+      if self.patten_file.closed:
+         raise StopIteration
+
+      line = self.patten_file.readline()
+      if not line:
+         self.patten_file.close()
+         raise StopIteration
+
+      pattern, search, replace = line.split(None, 3)
+      funcs = build_match_apply_function(
+         pattern, search, replace)
+      self.cache.append(funcs)
+      return funcs
+
+rules = LazyPlurRules()
+
+def plural(noun):
+   for match_rule, apply_rule in rules:
       if match_rule(noun):
          return apply_rule(noun)
 
+###################################################
+###################################################
 
-if __name__ == '__main__':
-   print(approximate_size(1000000000000, False))
-   print(approximate_size(1000000000000))
+class Fib:
+   def __init__(self, max):
+      self.max = max
+
+   def __iter__(self):
+      self.a = 0
+      self.b = 1
+      return self
+
+   def __next__(self):
+      fib = self.a
+      if fib > self.max:
+         raise StopIteration
+      self.a, self.b = self.b, self.a + self.b
+      return fib
+   
+class FibByIndex:
+   def __init__(self, index):
+      self.index = 0
+      self.index_to_find = index
+
+   def __iter__(self):
+      self.a = 0
+      self.b = 1
+      return self
+
+   def __next__(self):
+      if self.index > self.index_to_find:
+         raise StopIteration
+      self.a, self.b = self.b, self.a + self.b
+      self.index += 1
+      return self.a  
+
+###################################################
+###################################################
+
+
+if __name__ == '__main__':   
+   for w in words:
+      print(plural(w))
